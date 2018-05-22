64ae7f00b44da5c407a10c4efdde37795f56921d
agusosimani <aosimani@itba.edu.ar>
2018-05-22 11:03:09 -0300

    navigation bar

--- app/src/main/java/com/example/agusosimani/homely/Home.java
+++ app/src/main/java/com/example/agusosimani/homely/Home.java
@@ -17,14 +17,14 @@ public class Home extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
+                case R.id.navigation_routines:
+                    mTextMessage.setText("Routines");
+                    return true;
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_devices:
+                    mTextMessage.setText("Devices");
                     return true;
             }
             return false;
