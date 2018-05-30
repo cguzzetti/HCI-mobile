365492d9f50bc345708a22c3a205b45f50dc29e2
Unknown <igvidaurreta@gmail.com>
2018-05-29 22:04:12 -0300

    Estructura de la página de devices. Me faltan cosas todavía

--- app/src/main/java/com/example/agusosimani/Homely/DevicesActivity.java
+++ app/src/main/java/com/example/agusosimani/Homely/DevicesActivity.java
@@ -1,13 +1,25 @@
 package com.example.agusosimani.Homely;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class DevicesActivity extends AppCompatActivity {
+
+    private ArrayList<String> devices = new ArrayList<>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,5 +55,26 @@ public class DevicesActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.logo);
+
+        devices.add("Oven");
+        devices.add("AC");
+        devices.add("Johns lights");
+        devices.add("Living room lights");
+        devices.add("Johns computer");
+        devices.add("Hallway lights");
+
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(DevicesActivity.this, android.R.layout.simple_list_item_activated_1, devices);
+        final ListView listview = (ListView) findViewById(R.id.list_view);
+        if (listview != null){
+            listview.setAdapter(adapter);
+        }
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(DevicesActivity.this,"Activated",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
